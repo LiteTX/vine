57e01abac14690b8e3d0d3c23826cd1b25b26322
Your Name <you@example.com>
2022-04-10 12:38:30 +0100

    all

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword33 = 'mongodb://localhost:27017/mern2';
+dbPassword = 'mongodb+srv://admin:admin@cluster0.wbb6r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+module.exports = {
+    mongoURI: dbPassword
+};
